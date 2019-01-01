990de16ee7ba90a6a306ebbc4f7b8a0021910add
Vadim <zaytsewvk@gmail.com>
2019-01-01 19:32:44 +0200

    Fixed header menu. Added footer adaptive for phone.

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,18 @@ $('.header_burger').on('click', function(e) {
 	$('.header_active_logo-img').toggleClass('active');
 	$(this).toggleClass('header_burger_active');
 });
+
+$('.footer-mobile_allPages-openGroup').on('click', function() {
+	$(this).toggleClass('footer-mobile_allPages-openGroup_active');
+	$('.footer-mobile_allPages-links').toggleClass('active');
+})
+
+$('.footer-mobile_information-openGroup').on('click', function() {
+	$(this).toggleClass('footer-mobile_information-openGroup_active');
+	$('.footer-mobile_information-links').toggleClass('active');
+})
+
+$('.footer-mobile_socials-openGroup').on('click', function() {
+	$(this).toggleClass('footer-mobile_socials-openGroup_active');
+	$('.footer-mobile_socials-links').toggleClass('active');
+})
